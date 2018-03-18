86ddf9a5e5b70e823534a03349149614c56256dc
Anastasia Lukyanenko <vicooodin@gmail.com>
2018-03-18 13:52:32 +0200

    Without Paste into file

---
+++ Semester_6_MS_DOS/lab3/src/WindowProcedure.cpp
@@ -0,0 +1,168 @@
+//################################################
+//
+//    #             
+//    #  #           Program: HeaderMaker
+//    #  #          
+//    ####   #       Author: Anastasia Lukyanenko
+//    #  ## ##      
+//    #  # # #       Created By HeaderMaker 
+//       #   #      
+//           #      
+//
+//################################################
+
+#include "header_maker.h"
+
+LRESULT CALLBACK DialogProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch(msg)
+	{
+		case WM_COMMAND:
+			switch(wp)
+			{
+				case 1:
+					DestroyWindow(hWnd);
+					break;
+			}
+			break ;
+		case WM_CLOSE:
+			DestroyWindow(hWnd);
+			break ;
+		default:
+			return DefWindowProcW(hWnd, msg, wp, lp);
+	}
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	int 	i;
+	int 	j;
+	char 	*str_bord;
+	char 	*str_space;
+	int		val;
+
+	switch (msg)
+	{
+		case WM_COMMAND:
+			switch(wp)
+			{
+				case FILE_MENU_EXIT:
+					{
+						val = MessageBoxW(hWnd, L"Are you sure?", L"Exit",
+							MB_YESNO | MB_ICONQUESTION);
+						if (val == IDYES)
+							DestroyWindow(hWnd);
+						break;
+					}
+				case FILE_MENU_OPEN:
+					open_file(hWnd);
+					break ;
+				case FILE_MENU_SAVE:
+					save_file(hWnd);
+					break ;
+				case FILE_MENU_HELP:
+					displayDialog(hWnd);
+					break ;
+				case CLEAR_FIELD:
+				{
+					SetWindowText(hHeader, "");
+					break ;
+				}
+				case MAKE_HEADER:
+					i = 0;
+					prog_name = (char *)malloc(sizeof(char) * 101);
+					author = (char *)malloc(sizeof(char) * 101);
+					comment_type = (char *)malloc(sizeof(char) * 50);
+					header = (char *)malloc(sizeof(char) * 1001);
+					GetWindowText(hProg_name, prog_name, 100);
+					GetWindowText(hAuthor, author, 100);
+					GetWindowText(hComment_type, comment_type, 100);
+					
+					if (strcmp(prog_name, "") == 0 || strcmp(author, "") == 0
+						|| strcmp(comment_type, "") == 0)
+					{
+						val = MessageBoxW(hWnd, L"Some fields are empty.", L"Error",
+							MB_ABORTRETRYIGNORE | MB_ICONEXCLAMATION);
+						switch(val)
+						{
+							case IDABORT:
+								DestroyWindow(hWnd);
+								break ;
+							case IDRETRY:
+								return 0;
+							case IDIGNORE:
+								break ;
+						}
+					}
+					strcpy(header, comment_type);
+					
+					str_bord = (char *)malloc(sizeof(char) * 70);
+					str_space = (char *)malloc(sizeof(char) * 70);
+					while (j < 48)
+						str_bord[j++] = '#';
+					str_bord[j] = '\0';
+					j = 0;
+					while (j < 69)
+						str_space[j++] = ' ';
+					str_space[j] = '\0';
+					
+					while (i < 12)
+					{
+						if (i != 0)
+							strcat(header, comment_type);
+						if (i == 0 || i == 11)
+							strcat(header, str_bord);
+						if (i == 2)
+							strcat(header, "    #             ");
+						if (i == 3)
+						{
+							strcat(header, "    #  #          ");
+							strcat(header, " Program: ");
+							strcat(header, prog_name);
+						}
+						if (i == 4)
+							strcat(header, "    #  #          ");
+						if (i == 5)
+						{
+							strcat(header, "    ####   #      ");
+							strcat(header, " Author: ");
+							strcat(header, author);
+						}
+						if (i == 6)
+							strcat(header, "    #  ## ##      ");
+						if (i == 7)
+						{
+							strcat(header, "    #  # # #      ");
+							strcat(header, " Created By HeaderMaker ");
+						}
+						if (i == 8)
+							strcat(header, "       #   #      ");
+						if (i == 9)
+							strcat(header, "           #      ");
+						strcat(header, "\r\n");
+						i++;
+					}
+					SetWindowText(hHeader, header);
+					
+					free(str_space);
+					free(str_bord);
+					
+					free(prog_name);
+					free(author);
+					free(comment_type);
+					free(header);
+					break ;
+			}
+			break ;
+		case WM_CREATE:
+			loadImages();
+			AddMenus(hWnd);
+			AddControls(hWnd);
+			break ;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break ;
+		default:
+			return DefWindowProcW(hWnd, msg, wp, lp);
+	}
+}
---
+++ Semester_6_MS_DOS/lab3/src/header_maker.h
@@ -0,0 +1,57 @@
+//################################################
+//
+//    #             
+//    #  #           Program: HeaderMaker
+//    #  #          
+//    ####   #       Author: Anastasia Lukyanenko
+//    #  ## ##      
+//    #  # # #       Created By HeaderMaker 
+//       #   #      
+//           #      
+//
+//################################################
+
+#ifndef HEADER_MAKER_H
+#define HEADER_MAKER_H
+
+# include <windows.h>
+# include <stdlib.h>
+# include <wchar.h>
+# include <stdio.h>
+
+# define FILE_MENU_OPEN 1
+# define FILE_MENU_SAVE 2
+# define FILE_MENU_EXIT 3
+# define MAKE_HEADER 4
+# define CLEAR_FIELD 5
+# define FILE_MENU_HELP 6
+
+extern char		*prog_name;
+extern char		*author;
+extern char		*comment_type;
+extern char		*header;
+
+extern HWND		hProg_name;
+extern HWND		hAuthor;
+extern HWND		hComment_type;
+extern HWND		hHeader;
+extern HWND		hFile_cont;
+extern HWND		hSave;
+extern HWND		hOpen;
+
+extern HMENU	hMenu;
+extern HBITMAP	hSaveImg;
+extern HBITMAP	hOpenImg;
+
+LRESULT CALLBACK	WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+LRESULT CALLBACK	DialogProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+void				AddMenus(HWND);
+void				AddControls(HWND);
+void				loadImages();
+void				registerDialogClass(HINSTANCE hInst);
+void				displayDialog(HWND hWnd);
+void				open_file(HWND hWnd);
+void				display_file(char *path);
+void				write_file(char *path);
+void				save_file(HWND hWnd);
+#endif
